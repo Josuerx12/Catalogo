500c0396240ed293ca08e99b6f7d75b5c7be6054
josuerx12 <josueaze12@gmail.com>
2023-10-23 20:12:27 -0300

    Feat: Change password option in profile menu.

--- src/components/changePasswordModal/index.tsx
+++ src/components/changePasswordModal/index.tsx
@@ -1,24 +1,84 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 type props = {
   show: boolean;
   handleShow: () => void;
 };
 
+type newPass = {
+  password: null | string;
+  confirmPassword: null | string;
+};
+
 const ChangePasswordModal = ({ show, handleShow }: props) => {
+  const [newPassword, setNewPassword] = useState<newPass>({
+    password: null,
+    confirmPassword: null,
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    handleShow();
+  };
+
   return (
     <Modal show={show} onHide={handleShow} backdrop="static">
       <Modal.Header closeButton>
         <Modal.Title>Alterar senha do usuário</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Nova senha:</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              required
+              minLength={8}
+              onChange={(e) =>
+                setNewPassword((prev) => ({
+                  ...prev,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+            ></Form.Control>
+            <Form.Text className="text-muted">
+              Sua nova senha deve conter pelo menos 8 caracteres 1 caracter
+              especial e 1 letra maiúscula
+            </Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Confirme a nova senha:</Form.Label>
+            <Form.Control
+              name="confirmPassword"
+              type="password"
+              required
+              minLength={8}
+              onChange={(e) =>
+                setNewPassword((prev) => ({
+                  ...prev,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+            ></Form.Control>
+            <Form.Text className="text-muted">
+              {newPassword.confirmPassword !== newPassword.password
+                ? "As senhas devem ser iguais."
+                : "Senhas identicas"}
+            </Form.Text>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleShow}>
-          Close
+          Fechar
         </Button>
-        <Button variant="primary" onClick={handleShow}>
-          Save Changes
+        <Button variant="primary" onClick={handleSubmit}>
+          Confirmar
         </Button>
       </Modal.Footer>
     </Modal>
