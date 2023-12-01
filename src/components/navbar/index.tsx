a3b952bcf3497dd14633f51742288e6bc2e2bcef
Josue Silva de Azevedo de Carvalho <115432564+Josuerx12@users.noreply.github.com>
2023-12-01 10:49:30 -0300

    fix: Remove unused import statement.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Auth } from "../../context/authContext";
 const Navmenu = () => {
   const navigate = useNavigate();
