c3d7fa9bde41c25e82619a966f5daa7d6b79ca5b
josuerx12 <josueaze12@gmail.com>
2023-12-04 14:51:29 -0300

    feat: Remove unsual import.

--- src/pages/userProfile/index.tsx
+++ src/pages/userProfile/index.tsx
@@ -1,6 +1,3 @@
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { HiPencilAlt } from "react-icons/hi";
 import { BsPersonFillLock } from "react-icons/bs";
@@ -19,88 +16,76 @@ const User = () => {
   const handleChangePasswordModal = () =>
     setShowChangePasswordModal((prev) => !prev);
   return (
-    <Container className="bg-light rounded p-2 w-100 m-2">
+    <div
+      className="d-flex flex-column justify-content-center align-items-center bg-light rounded mx-auto p-2 m-2"
+      style={{ width: "90%" }}
+    >
       <EditUserModal handleShow={handleShowEditModal} show={showEditModal} />
       <ChangePasswordModal
         show={showChangePasswordModal}
         handleShow={handleChangePasswordModal}
       />
       <h3 className="text-center mt-4 mb-4 text-dark">Perfil do usuário</h3>
-      <Col>
-        <Row className="justify-content-center ">
-          {loadingPhoto && (
-            <img
-              className="rounded shadow bg-body"
-              style={{
-                width: "14rem",
-                aspectRatio: "1",
-              }}
-              src="/carregando-1.gif"
-            />
-          )}
-          <img
-            style={
-              loadingPhoto
-                ? { display: "none" }
-                : { width: "14rem", height: "12rem" }
-            }
-            className="rounded shadow bg-body p-0"
-            src={
-              user?.photo
-                ? `https://userphotoscatalogo.s3.us-east-2.amazonaws.com/${user.photo}`
-                : "/no-profile.jpg"
-            }
-            onLoad={handlePhotoLoading}
-          />
-        </Row>
-        <Row className="justify-content-md-center">
-          <div
-            className="d-flex flex-column align-items-center border p-4 mb-4 mt-4 bg-white rounded-3"
-            style={{ width: "fit-content" }}
-          >
-            <div>
-              <p
-                className="text-dark text-start"
-                style={{ fontSize: "1.2rem" }}
-              >
-                <b>ID do usuário:</b> {user?._id}{" "}
-              </p>
-              <p
-                className="text-dark text-start"
-                style={{ fontSize: "1.2rem" }}
-              >
-                <b>Nome:</b> {user?.name}{" "}
-              </p>
-              <p
-                className="text-dark text-start"
-                style={{ fontSize: "1.2rem" }}
-              >
-                <b>E-mail:</b> {user?.email}{" "}
-              </p>
-              <div className="d-flex gap-3 justify-content-center">
-                <Button
-                  variant="success"
-                  className="d-flex align-items-center justify-content-center gap-1"
-                  style={{ width: "100%", fontSize: "1.2rem" }}
-                  onClick={handleShowEditModal}
-                >
-                  Editar usuário <HiPencilAlt className="text-light" />
-                </Button>
-                <Button
-                  variant="primary"
-                  className="d-flex align-items-center justify-content-center gap-1"
-                  style={{ width: "100%", fontSize: "1.2rem" }}
-                  onClick={handleChangePasswordModal}
-                >
-                  Trocar senha
-                  <BsPersonFillLock />
-                </Button>
-              </div>
-            </div>
+      {loadingPhoto && (
+        <img
+          className="rounded shadow bg-body"
+          style={{
+            width: "14rem",
+            aspectRatio: "1",
+          }}
+          src="/carregando-1.gif"
+        />
+      )}
+      <img
+        style={
+          loadingPhoto
+            ? { display: "none" }
+            : { width: "14rem", height: "12rem" }
+        }
+        className="rounded shadow bg-body p-0"
+        src={
+          user?.photo
+            ? `https://userphotoscatalogo.s3.us-east-2.amazonaws.com/${user.photo}`
+            : "/no-profile.jpg"
+        }
+        onLoad={handlePhotoLoading}
+      />
+      <div
+        className="w-100 d-flex flex-column align-items-center border p-4 mb-4 mt-4 bg-white rounded-3"
+        style={{ width: "fit-content" }}
+      >
+        <div>
+          <p className="text-dark text-start" style={{ fontSize: "1.2rem" }}>
+            <b>ID do usuário:</b> {user?._id}{" "}
+          </p>
+          <p className="text-dark text-start" style={{ fontSize: "1.2rem" }}>
+            <b>Nome:</b> {user?.name}{" "}
+          </p>
+          <p className="text-dark text-start" style={{ fontSize: "1.2rem" }}>
+            <b>E-mail:</b> {user?.email}{" "}
+          </p>
+          <div className="d-flex gap-3 flex-wrap justify-content-start">
+            <Button
+              variant="success"
+              className="d-flex align-items-center justify-content-center gap-1"
+              style={{ fontSize: "1.2rem" }}
+              onClick={handleShowEditModal}
+            >
+              Editar usuário <HiPencilAlt className="text-light" />
+            </Button>
+            <Button
+              variant="primary"
+              className="d-flex align-items-center justify-content-center gap-1"
+              style={{ fontSize: "1.2rem" }}
+              onClick={handleChangePasswordModal}
+            >
+              Trocar senha
+              <BsPersonFillLock />
+            </Button>
           </div>
-        </Row>
-      </Col>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 
