0e7b5c95db5a851e043562b036506880224ab404
josuerx12 <josueaze12@gmail.com>
2024-06-03 09:04:50 -0300

    feat: new api url added.

--- src/config/apiConnection.ts
+++ src/config/apiConnection.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://catalogoapi.josuecarvalho.cloud";
+const url = "https://catalogo-api-theta.vercel.app/";
 
 export const api = axios.create({
   baseURL: url,
