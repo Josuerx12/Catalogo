cdb74365681869c89f4dc619bb12440e28d008f9
josuerx12 <josueaze12@gmail.com>
2024-07-15 16:32:36 -0300

    feat: addition to react query provider.

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,27 @@ import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.tsx";
-import { ProductProvider } from "./context/productsContext.tsx";
 import { AdminProvider } from "./context/adminContext.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <ProductProvider>
+      <QueryClientProvider client={client}>
         <AdminProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
         </AdminProvider>
-      </ProductProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
